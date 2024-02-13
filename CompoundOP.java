eb9fba4e6e7ccbfb46c8fcdd2396fbc446adfa12
Sujatadhere <159412649+Sujatadhere@users.noreply.github.com>
2024-02-13 22:47:49 +0530

    Add files via upload

---
+++ CompoundOP.java
@@ -0,0 +1,16 @@
+ class CompoundOP {
+    public static void main (String[]args)
+    {
+    
+    float age1=3.5f;
+    int age=15;
+    //compound assigment operator 
+    age+=age1;
+    System.out.println(age);
+}
+}
+/*output:
+ * compile time successful
+ * runtime successful
+ * 18
+ */
