fd343181de98b642414de2e19180782f59d5f5e0
Sujatadhere <159412649+Sujatadhere@users.noreply.github.com>
2024-02-16 17:31:50 +0530

    Add files via upload

---
+++ DoubleToChar.java
@@ -0,0 +1,15 @@
+public class DoubleToChar {
+    public static void main(String[] args) {
+        double b=1024.25;
+        char CH = (char)b;
+        System.out.println(CH);
+    }
+    
+}
+ 
+/*Output:
+compile time success
+runtime time success
+?
+ * 
+ */
