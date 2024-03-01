e278bdbed822d1b9f2a98465b4cbce2b2700699e
Sujatadhere <159412649+Sujatadhere@users.noreply.github.com>
2024-03-01 13:33:00 +0530

    Add files via upload

---
+++ ExampleWhatIf.java
@@ -0,0 +1,17 @@
+ class ExampleWhatIf {
+    public static void main (String[]args){
+        int age = 20;
+        if (age>=18);
+        {
+            System.out.println("congrats");
+        }
+    }
+    
+}
+
+/*OUTPUT:
+compile time success
+runtime sucess
+congrats
+ * 
+ */
