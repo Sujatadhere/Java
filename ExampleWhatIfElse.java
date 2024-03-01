3d81a8ce18eec492a5ce83e5c6509aeb85cad614
Sujatadhere <159412649+Sujatadhere@users.noreply.github.com>
2024-03-01 13:33:52 +0530

    Add files via upload

---
+++ ExampleWhatIfElse.java
@@ -0,0 +1,16 @@
+ class ExampleWhatIfElse {
+    public static void main(String[] args) {
+        int money = 150;
+        if(money>=120);
+        {
+            System.out.println("congrats you can buy tickets");
+        }
+        else
+        {
+            System.out.println("make some more money");
+        }
+
+        }
+        }
+    
+
