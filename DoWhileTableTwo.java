74b4fced8bf9535753e6452e350d7ab2c95ae292
Sujatadhere <159412649+Sujatadhere@users.noreply.github.com>
2024-03-05 21:32:34 +0530

    Add files via upload

---
+++ DoWhileTableTwo.java
@@ -0,0 +1,27 @@
+public class DoWhileTableTwo {
+    public static void main(String[] args) {
+        int a=1;
+        do
+        {
+            System.out.println(2*a);
+            a++;
+        }while(a<=10);
+            
+        }
+    }
+    
+/*
+ * Output:
+ * compile time success
+ * runtime success
+ * 2
+ * 4
+ * 6
+ * 8
+ * 10
+ * 12
+ * 14
+ * 16
+ * 18
+ * 20
+ */
