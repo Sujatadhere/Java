747b2a20e78a18333c8b688bfec128c0c7b993cb
Sujatadhere <159412649+Sujatadhere@users.noreply.github.com>
2024-03-05 21:31:18 +0530

    Add files via upload

---
+++ DoWhileExample1.java
@@ -0,0 +1,27 @@
+public class DoWhileExample1 {
+    public static void main(String[] args) {
+        //declaration
+        int a= 1;
+        do
+        {
+            System.out.println(a);
+           // update
+            a++;
+        }while(a<=10); //condition
+      }
+    }
+    
+    /*Output:
+    compile time success
+    runtime success
+    1
+    2
+    3
+    4
+    5
+    6
+    7
+    8
+    9
+    10
+     */
