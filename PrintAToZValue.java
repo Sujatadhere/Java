c1d1f4dc446ea35a3de23bb5287a6b900bfe18c1
Sujatadhere <159412649+Sujatadhere@users.noreply.github.com>
2024-03-09 15:02:08 +0530

    Add files via upload

---
+++ PrintAToZValue.java
@@ -0,0 +1,44 @@
+public class PrintAToZValue {
+    public static void main(String[] args) {
+        char a = 'A';
+    while(a<='Z')
+    {
+        System.out.println(a);
+        a++;
+    }
+}
+        
+    }
+    
+/*
+ * Output:
+ * compile time success
+ * runtime success
+ * A
+ * B
+ * C
+ * D
+ * E
+ * F
+ * G
+ * H
+ * I
+ * J
+ * K
+ * L
+ * M
+ * N
+ * O
+ * P
+ * Q
+ * R
+ * S
+ * T
+ * U
+ * V
+ * W
+ * X
+ * Y
+ * Z 
+ */
+
