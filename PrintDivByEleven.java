2acacd40b4f25f5b1ed908af33d123416ca301f9
Sujatadhere <159412649+Sujatadhere@users.noreply.github.com>
2024-03-09 15:08:43 +0530

    Add files via upload

---
+++ PrintDivByEleven.java
@@ -0,0 +1,40 @@
+public class PrintDivByEleven {
+    public static void main(String[] args) {
+        int a = 100;
+        do{
+            if(a%11==0)
+            {
+            System.out.println(a);
+            }
+            a++;
+           }while(a<=300);
+
+        }
+        
+    }
+
+/* 
+Output:
+compile time success
+runtime success
+110
+121
+132
+143
+154
+165
+176
+187
+198
+209
+220
+231
+242
+253
+264
+275
+286
+297
+*/
+    
+
