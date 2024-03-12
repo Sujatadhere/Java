6107dfd02696443c9067504d4b04111f25974f0c
Sujatadhere <159412649+Sujatadhere@users.noreply.github.com>
2024-03-12 16:57:06 +0530

    Add files via upload

---
+++ PrintFirstTenEvenNumber.java
@@ -0,0 +1,39 @@
+public class PrintFirstTenEvenNumber {
+    public static void main(String[] args) {
+        int i=0;
+        int j=0;
+       
+        while(true){
+            if(j%2==0)
+            {
+                i++;
+                System.out.println(j);
+            }
+                j++;
+                if(i==10){
+                    break;
+                }
+                }
+            }
+        }
+        
+        /*
+         * Output:
+         * compile time success
+         * runtime success
+         * 0
+         * 2
+         * 4
+         * 6
+         * 8
+         * 10
+         * 12
+         * 14
+         * 16
+         * 18
+         * 
+         */
+        
+    
+    
+
