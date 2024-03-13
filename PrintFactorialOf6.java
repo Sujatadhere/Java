77c71cdcced37d8dc05b77ec76c4eb900ca4c7f5
Sujatadhere <159412649+Sujatadhere@users.noreply.github.com>
2024-03-13 06:11:29 +0530

    Add files via upload

---
+++ PrintFactorialOf6.java
@@ -0,0 +1,25 @@
+public class PrintFactorialOf6 {
+    public static void main(String[] args) {
+        int i=6;
+        int fact=1;
+        while(i>0){
+            fact=fact*i;
+            i--;
+        }
+        System.out.println(fact);   
+        
+        }
+
+        }
+
+        /*
+         * Output:
+         * compile time successful
+         * run time successful
+         * 720
+         */
+        
+        
+    
+    
+
